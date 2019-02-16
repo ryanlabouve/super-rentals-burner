4c9f21b3959cba2a08e3799188824407b94ee056
Ryan LaBouve <imryanlabouve@gmail.com>
2019-02-16 00:11:23 -0600

    super rentals tutorial

--- app/router.js
+++ app/router.js
@@ -3,10 +3,15 @@ import config from './config/environment';
 
 const Router = EmberRouter.extend({
   location: config.locationType,
-  rootURL: config.rootURL
+  rootURL: config.rootURL,
 });
 
 Router.map(function() {
+  this.route('about');
+  this.route('contact');
+  this.route('rentals', function() {
+    this.route('show', {path: '/:rental_id'});
+  });
 });
 
 export default Router;
---
+++ tests/integration/components/location-map-test.js
@@ -0,0 +1,10 @@
+import {module, test} from 'qunit';
+import {setupRenderingTest} from 'ember-qunit';
+
+module('Integration | Component | location-map', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    assert.ok('todo');
+  });
+});
