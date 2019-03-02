cee8427d1d87784dc5d791d75e274e3cf9804d81
Ryan LaBouve <imryanlabouve@gmail.com>
2019-03-02 17:37:46 -0600

    Tweak mirage

--- mirage/config.js
+++ mirage/config.js
@@ -15,6 +15,6 @@ export default function() {
 
   // Find and return the provided rental from our rental list above
   this.get('/rentals/:id', function(db, request) {
-    return {data: rentals.find(rental => request.params.id === rental.id)};
+    return db.rentals.find(request.params.id);
   });
 }
